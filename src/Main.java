f2a33db7463e0f932a0239405bf79548d0096126
Izzy Ehnes <izzyehnes@gmail.com>
2020-12-03 12:33:43 -0800

    Feature: Add Node class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,73 @@
+class Node
+{
+    private int value;
+    private int height;
+    private Node right;
+    private Node left;
+
+
+
+    public Node(int inValue)
+    {
+        this.value = inValue;
+        left = null;
+        right = null;
+    }
+
+
+
+    public int getValue()
+    {
+        return this.value;
+    }
+
+
+
+    public void setLeft(Node inNode)
+    {
+        left = inNode;
+    }
+
+
+
+    public Node getLeft()
+    {
+        return left;
+    }
+
+
+
+    public void setRight(Node inNode)
+    {
+        right = inNode;
+    }
+
+
+
+    public Node getRight()
+    {
+        return right;
+    }
+
+
+
+    public int getHeight()
+    {
+        return this.height;
+    }
+
+
+
+    public void setHeight(int inHeight)
+    {
+        this.height = inHeight;
+    }
+}
+
+
+
+
+
 public class Main
 {
 
